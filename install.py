5428773a0bf3c43517fed48728111bbf3d1fd817
Sergey M <s.mereskin@yandex.ru>
2026-02-06 10:21:45 +0300

    Добавил в install.py настройку окружения для библиотек python и установку нужны библиотек Linux

--- install.py
+++ install.py
@@ -1,8 +1,78 @@
 import os
+import sys
+from subprocess import run
 
 # Проверка есть ли requirments
 all_items = os.listdir(__file__)
 allScriptNeed = ["requirments.txt"]
 
+is_all_files_exists = True
+
 for item in allScriptNeed:
-    if not in os.listDir(all_items)
+    if item not in os.listDir(all_items):
+        print(f"[-] Файл {item} отсутствует")
+        is_all_files_exists = False
+
+if not is_all_files_exists:
+    print("[-] Отстуствуют необходимые файлы.")
+    sys.exit()
+
+# Установка необходимых пакетов
+
+# Обновление доступных пакетов
+try:
+    print("[~] Обновление информации о возможности обновления доступных пакетов")
+    result = run(["sudo", "apt", "update"])
+    if result.stderr == 0:
+        print("[+] Обновлен список доступных пакетов и их версий")
+except Exception as PackagesUpdateError:
+    print(f"[-] Обновление списка доступных пакетов и их версий завершилось с ошибкой:\n{PackagesUpdateError}")
+
+# Необходимые пакеты для корректной работы системы
+packages = [
+    'curl'
+]
+
+# Установка пакетов из списка
+for package in packages:
+    try:
+        print(f"[~] Установка пакета: {package}")
+        packageInstallResult = run(["apt", "install", package], capture_output=True)
+        if packageInstallResult.stderr == 0:
+            print(f"[+] Пакет {package} успешно был установлен")
+    except Exception as PackageInstallError:
+        print(f"[-] Установка пакета {package} завершилась с ошибкой:\n{PackageInstallError}")
+
+# Установка необходимых библиотек из requirments.txt
+
+# Создание виртаульного оркужения
+try:
+    print("[~] Создание виртуального окружения")
+    venvCreationResult = run(["python3", "-m", "venv", "venv"], capture_output=True)
+    if venvCreationResult.stderr == 0:
+        print("[+] Виртуальное окружение успешно создано")
+except Exception as venvCreationError:
+    print(f"[-] При создании виртуального окружения произошла ошибка:\n{venvCreationError}")
+    sys.exit()
+
+# Активация виртуального окружения
+try:
+    print("[~] Активация виртуального окружения")
+    run(["source", "venv/bin/activate"])
+except Exception as venvActivationError:
+    print(f"[-] При активации виртуального окружения произошла ошибка:\n{venvCreationError}")
+    sys.exit()
+
+# Установка зависимостей
+try:
+    print("[~] Установка зависимостей requirements.txt")
+    reqInstallResults = run(["pip", "isntall", "-r", "requirements.txt"], capture_output=True)
+    if reqInstallResults.stderr == 0:
+        print("[+] Зависимости из requirements.txt были успешно установлены")
+except Exception as reqInstallError:
+    print(f"[-] При установке зависимостей requirements.txt произошла ошибка:\n{reqInstallResults}")
+    sys.exit()
+
+# Вывод имеющихся библиотек
+currentPythonLibrary = run(["pip", "list"], capture_output=True, text=True)
+print(currentPythonLibrary.stdout)
